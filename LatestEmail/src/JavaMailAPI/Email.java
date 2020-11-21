c140676d72de8a1bb3876ef06968a2672060c078
SudhaAmarnath <sudha04.a@gmail.com>
2020-11-20 19:53:39 -0800

    initial commit

---
+++ LatestEmail/src/JavaMailAPI/Email.java
@@ -0,0 +1,40 @@
+package JavaMailAPI;
+
+public class Email {
+	private String from;
+	private String to;
+	private String subject;
+	private byte[] body;
+
+	public String getFrom() {
+		return from;
+	}
+
+	public void setFrom(String from) {
+		this.from = from;
+	}
+
+	public String getTo() {
+		return to;
+	}
+
+	public void setTo(String to) {
+		this.to = to;
+	}
+
+	public String getSubject() {
+		return subject;
+	}
+
+	public void setSubject(String subject) {
+		this.subject = subject;
+	}
+
+	public byte[] getBody() {
+		return body;
+	}
+
+	public void setBody(byte[] body) {
+		this.body = body;
+	}
+}
